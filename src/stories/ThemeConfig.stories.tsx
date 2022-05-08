1006e50ea70ce823ec87e3b11c2c127f4f974094
Vinh Hoang <vinh.hoang@employmenthero.com>
2022-05-08 17:28:02 +0700

    Theme config doc

---
+++ src/stories/ThemeConfig.stories.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react';
+import { Meta, Story } from '@storybook/react';
+import Button from '../components/Button';
+
+type ThemeConfigProps = {
+  primaryColor: string;
+  secondaryColor: string;
+  dangerColor: string;
+};
+
+const ThemeConfig: React.FC<ThemeConfigProps> = ({
+  primaryColor,
+  secondaryColor,
+  dangerColor,
+}) => {
+  useEffect(() => {
+    document.documentElement.style.setProperty('--primary-color', primaryColor);
+  }, [primaryColor]);
+
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      '--secondary-color',
+      secondaryColor
+    );
+  }, [secondaryColor]);
+
+  useEffect(() => {
+    document.documentElement.style.setProperty('--danger-color', dangerColor);
+  }, [dangerColor]);
+
+  return (
+    <div>
+      <Button text="Primary" variant="primary" />
+      <Button text="Secondary" variant="secondary" />
+      <Button text="Danger" variant="danger" />
+    </div>
+  );
+};
+
+export default {
+  title: 'ThemeConfigs/Colors',
+  argTypes: {},
+} as Meta<typeof ThemeConfig>;
+
+const Template: Story<ThemeConfigProps> = (args) => <ThemeConfig {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  primaryColor: '#3b82f6',
+  secondaryColor: '#a855f7',
+  dangerColor: '#f43f5e',
+};
